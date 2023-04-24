8712776920c05293f4e3d5c0cca516fe6063f5a5
white <white@SYNIS>
2023-04-24 22:38:47 +0900

    초기 커밋.

---
+++ Test1/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Test1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(410, 72);
+            button1.Name = "button1";
+            button1.Size = new Size(360, 223);
+            button1.TabIndex = 0;
+            button1.Text = "버튼";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("맑은 고딕", 72F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(68, 122);
+            label1.Name = "label1";
+            label1.Size = new Size(299, 128);
+            label1.TabIndex = 1;
+            label1.Text = "라벨1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+    }
+}

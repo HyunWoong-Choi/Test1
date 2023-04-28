acae5273a0281c36a2b7ca6cbb6c176005375ded
최현웅 <whiteblueter@gmail.com>
2023-04-28 22:15:39 +0900

    클래스 부분 변수 선언

---
+++ Test1/ClassA.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    internal class 밀키트
+    {
+        string 감자;
+        string 고구마;
+        string 김치;
+
+        
+
+
+    }
+}
--- Test1/Form1.cs
+++ Test1/Form1.cs
@@ -1,5 +1,3 @@
-using System.Windows.Forms.VisualStyles;
-
 namespace Test1
 {
     public partial class Form1 : Form
@@ -13,8 +11,41 @@ namespace Test1
         {
             label1.Text = "Test";
 
-            
+            밀키트 A세트 = new 밀키트(); // 밀키트 A세트 = new 밀키트();
+            밀키트 B세트 = new 밀키트(); // 밀키트 A세트 = new 밀키트();
+            밀키트 C세트 = new 밀키트(); // 밀키트 A세트 = new 밀키트();
+
+
+            /*
+             * 데이터는 왠만하면 아는거 그냥 써라
+             * bool, int, string
+             * 
+             * 조작하는 방법은 4+2 6개가 있음
+             * 
+             * CRUd
+             * 조건 if()
+             * 반복 foreach ( var ~~ in ~~ )
+             * 
+             * 데이터를 뭉태기로 만들고 싶으면
+             * class 를 사용한다
+             * 
+             * class 이름 {
+             *      데이터1
+             *      데이터2
+             *      데이터3
+             * }
+             * 
+             * 
+             */
+
+
 
         }
+
+
+
+
+
+
     }
 }

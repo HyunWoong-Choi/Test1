95759543b2cd2b2d054126299884302fc5b4776e
최현웅 <whiteblueter@gmail.com>
2023-04-30 16:37:03 +0900

    Linq 까지 함

--- Test1/Form1.cs
+++ Test1/Form1.cs
@@ -1,3 +1,5 @@
+using System.Security.Cryptography.X509Certificates;
+
 namespace Test1
 {
     public partial class Form1 : Form
@@ -7,45 +9,44 @@ namespace Test1
             InitializeComponent();
         }
 
+
+
         private void button1_Click(object sender, EventArgs e)
         {
-            label1.Text = "Test";
+            List<int> IDs = new List<int>();
 
-            밀키트 A세트 = new 밀키트(); // 밀키트 A세트 = new 밀키트();
-            밀키트 B세트 = new 밀키트(); // 밀키트 A세트 = new 밀키트();
-            밀키트 C세트 = new 밀키트(); // 밀키트 A세트 = new 밀키트();
+            // 틀 안에 값을 채워
+            // 학생 데이터를 채움
+            IDs.Add(1);
+            IDs.Add(2);
 
-
-            /*
-             * 데이터는 왠만하면 아는거 그냥 써라
-             * bool, int, string
-             * 
-             * 조작하는 방법은 4+2 6개가 있음
-             * 
-             * CRUd
-             * 조건 if()
-             * 반복 foreach ( var ~~ in ~~ )
-             * 
-             * 데이터를 뭉태기로 만들고 싶으면
-             * class 를 사용한다
-             * 
-             * class 이름 {
-             *      데이터1
-             *      데이터2
-             *      데이터3
-             * }
-             * 
-             * 
-             */
+            var SelectedID = (from ID in IDs
+                             where ID >= 5
+                             where ID <= 8 // 어떠어떤 조건인 데이터를
+                             select ID).Take(3);                // 선택해서 가져와
 
 
+            label1.Text = String.Join(", ", SelectedID); // 과연 뭐가 나올까
 
         }
 
 
 
 
-
+        /*
+         * 변수 만드는거, 변수 값 수정하는거(변수 = ), 변수 값을 읽는거 ( = 변수)
+         * 
+         * if, foreach
+         * (else 쓰지마셈, for 쓰지마셈) - 근데 걍 무시하고 써도 되긴함
+         * 
+         * 
+         * class : 데이터(+처리)묶음. 데이터 묶음을 한번에 생성하는 용도 
+         * 
+         *  -> 수정, 읽는거, 삭제 : 못함....
+         *  -> 함수 (+오버로딩 +FP)
+         * 
+         * 
+         */
 
     }
 }

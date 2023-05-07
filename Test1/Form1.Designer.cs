09f52334e6de7c62cf01fc6e56ab481f1d08f553
최현웅 <whiteblueter@gmail.com>
2023-05-07 15:47:08 +0900

    전사 구현 완료

--- Test1/Form1.Designer.cs
+++ Test1/Form1.Designer.cs
@@ -30,6 +30,10 @@
         {
             button1 = new Button();
             label1 = new Label();
+            button2 = new Button();
+            button3 = new Button();
+            label2 = new Label();
+            label3 = new Label();
             SuspendLayout();
             // 
             // button1
@@ -38,9 +42,9 @@
             button1.ForeColor = Color.DarkGoldenrod;
             button1.Location = new Point(410, 72);
             button1.Name = "button1";
-            button1.Size = new Size(360, 223);
+            button1.Size = new Size(402, 126);
             button1.TabIndex = 0;
-            button1.Text = "버튼";
+            button1.Text = "케릭터생성";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
@@ -48,17 +52,61 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("맑은 고딕", 72F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(68, 122);
+            label1.Location = new Point(57, 70);
             label1.Name = "label1";
             label1.Size = new Size(299, 128);
             label1.TabIndex = 1;
             label1.Text = "라벨1";
             // 
+            // button2
+            // 
+            button2.Location = new Point(848, 72);
+            button2.Name = "button2";
+            button2.Size = new Size(234, 126);
+            button2.TabIndex = 2;
+            button2.Text = "불러오기";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(616, 277);
+            button3.Name = "button3";
+            button3.Size = new Size(286, 103);
+            button3.TabIndex = 3;
+            button3.Text = "공격";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("맑은 고딕", 72F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(79, 229);
+            label2.Name = "label2";
+            label2.Size = new Size(299, 128);
+            label2.TabIndex = 4;
+            label2.Text = "라벨1";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("맑은 고딕", 72F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(79, 390);
+            label3.Name = "label3";
+            label3.Size = new Size(299, 128);
+            label3.TabIndex = 5;
+            label3.Text = "라벨1";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1241, 562);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(button3);
+            Controls.Add(button2);
             Controls.Add(label1);
             Controls.Add(button1);
             Name = "Form1";
@@ -71,5 +119,9 @@
 
         private Button button1;
         private Label label1;
+        private Button button2;
+        private Button button3;
+        private Label label2;
+        private Label label3;
     }
 }
--- Test1/Form1.cs
+++ Test1/Form1.cs
@@ -5,17 +5,42 @@ namespace Test1
 {
     public partial class Form1 : Form
     {
+        전사 버섯;
+        전사 식식;
+        전사 내꺼;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-
-
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            내꺼 = new 전사(100, 20);
+            내꺼.Name = "내꺼";
+
+            // 식식이
+            식식 = new 전사(100, 20);
+            식식.Name = "식식";
+
+            // 밍밍이
+            버섯 = new 전사(90, 10);
+            버섯.Name = "버섯";
         }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = 내꺼.HP.ToString();
+            label2.Text = 식식.HP.ToString();
+            label3.Text = 버섯.HP.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            식식.공격(버섯);
+        }
     }
+
+
+
 }
